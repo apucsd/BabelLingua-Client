ab8daa243072f61ae64ead0117f83b909ffb28dc
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-06-09 00:11:23 +0600

    added React Awesome Reveal to section title

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-
+import { Fade } from "react-awesome-reveal";
 const SectionTitle = ({ heading, subheading }) => {
   return (
     <div className="flex justify-center items-center my-10">
-      <div>
-        <h1 className="font-bold uppercase  text-3xl border-x-4 border-primary p-2 inline-block w-auto">
-          {heading}
-        </h1>
-        <p className="text-sm text-center capitalize text-secondary italic">
-          {subheading}
-        </p>
-      </div>
+      <Fade direction="up">
+        <div>
+          <h1 className="font-bold uppercase  text-3xl border-x-4 border-primary p-2 inline-block w-auto">
+            {heading}
+          </h1>
+          <p className="text-sm text-center capitalize text-secondary italic">
+            {subheading}
+          </p>
+        </div>
+      </Fade>
     </div>
   );
 };
